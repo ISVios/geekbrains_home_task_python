19f4701cf3f00d74a6c333abf35e2ea62b2b6223
Vios <i.svoboda.vios@gmail.com>
2021-11-26 17:43:56 +0300

    fix task 2

--- Zhemanov_Alexander_dz_1/task_1_2.py
+++ Zhemanov_Alexander_dz_1/task_1_2.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
 """output:
-    sum of odd elem which % 7
+    sum of odd elem(xyz..) sum(x+y+z) which % 7
     sum of odd (elem + 17) which % 7
 """
+
+def sum_of_num(num:int):
+    res = 0
+
+    while num > 0:
+        res += num % 10
+        num //= 10
+    return res
 
 # lazy list
 list_of_odd = range(1, 1000 + 1, 2)
@@ -16,10 +24,10 @@ for elem in list_of_odd:
     cube = elem ** 3
     cube_with_17 = (elem + 17) ** 3
 
-    if cube % 7 == 0:
+    if sum_of_num(cube) % 7 == 0:
         sum_without_17 += cube
 
-    if cube_with_17 % 7 == 0:
+    if sum_of_num(cube_with_17) % 7 == 0:
         sum_with_17 += cube_with_17
 
 print("сумма без 17:", sum_without_17)
