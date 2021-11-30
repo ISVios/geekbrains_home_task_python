70275e80306d26113ef836309c07c82942684da5
Vios <i.svoboda.vios@gmail.com>
2021-11-30 16:59:04 +0300

    fix task 4

--- Zhemanov_Alexandr_dz_2/task_2_4.py
+++ Zhemanov_Alexandr_dz_2/task_2_4.py
@@ -6,7 +6,6 @@ input_list = ['инженер-конструктор Игорь',
             'главный бухгалтер МАРИНА',
             'токарь высшего разряда нИКОЛАй',
             'директор аэлита']
-answer = {}
 
 for string in input_list:
     correct_name = string.split()[-1].capitalize()
