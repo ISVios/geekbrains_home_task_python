7602ab6d9ab7ac1ec8fb981695135ce8e3488f19
Vios <i.svoboda.vios@gmail.com>
2021-11-30 16:47:51 +0300

    complete (1)

---
+++ Zhemanov_Alexandr_dz_2/task_2_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""" find what is type """
+
+print(f"type(14 * 3) is {type(15 * 3)}")
+print(f"type(15 / 3) is {type(15 / 3)}")
+print(f"type(15 // 3) is {type(15 // 3)}")
+print(f"type(15 ** 3) is {type(15 ** 3)}")
+
+# EXPEREMENT
+# expressions_list = ["type(14 * 3)", "type(14 / 3)", "type(14 // 3)", "type(14 ** 3)"]
+#
+# for exp in expressions_list:
+#     print(f"{exp} is {type(eval(exp))}") # 'eval' is dangerous method don't use
