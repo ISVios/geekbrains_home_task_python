ab8ae758e5c65481180eced87ca37d4298afcfa4
Vios <i.svoboda.vios@gmail.com>
2021-12-12 15:53:21 +0300

    dev task (3)(4)

---
+++ Zhemanov_Alexandr_dz_6/myutils.py
@@ -0,0 +1,9 @@
+
+""" my utils mode """
+
+def my_zip_gen(a_iter, b_iter):
+
+    len_b = len(b_iter)
+
+    return ((a_elem, b_iter[i]) if i < len_b else (a_elem, None)
+            for i, a_elem in enumerate(a_iter))
