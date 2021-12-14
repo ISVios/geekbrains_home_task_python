3ff402faf5e98665d62574e6963854584890d503
Vios <i.svoboda.vios@gmail.com>
2021-12-14 21:56:20 +0300

    Complete (6)

--- Zhemanov_Alexandr_dz_6/task_6_6/add_price.py
+++ Zhemanov_Alexandr_dz_6/task_6_6/add_price.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
 
     import sys
 
-    input_prices = list(map(lambda y:  f"{float(y):.2f}", filter(
+    input_prices = list(map(lambda y:  f"{float(y):100.2f}", filter(
         lambda x: x.replace(".", "").isdigit(), sys.argv[1:])))
+
     with open(PRICE_FILE, "a", encoding="utf-8") as price_list:
         print(*input_prices, sep="\n", file=price_list)
