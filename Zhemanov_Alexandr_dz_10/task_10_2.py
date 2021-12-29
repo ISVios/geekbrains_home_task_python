6e85c59f971c6a29ec2eabea0e4635a69018aa65
Vios <i.svoboda.vios@gmail.com>
2021-12-29 18:44:48 +0300

    Fix task_2(correct task)

--- Zhemanov_Alexandr_dz_10/task_10_2.py
+++ Zhemanov_Alexandr_dz_10/task_10_2.py
@@ -3,47 +3,103 @@
 # Todo: remake
 """ task 2 """
 
-from enum import Enum
 from abc import ABC, abstractmethod
 
 
-class ClothType(Enum):
-    """ type of Cloth """
-    COAT = 0
-    COSTUME = 1
+class MaterialLenSolver:
+    __length: float = 0
+    __coat_list = []
+    __costume_list = []
+
+    @property
+    def length(self):
+        return self.__length
+
+    @property
+    def node_list(self):
+        return {"coats": self.__coat_list, "costumies": self.__costume_list}
+
+    @node_list.setter
+    def node_list(self, other, add=True):
+        if other.__class__ is Coat:
+            if add:
+                self.__coat_list.append(other.get_name())
+            else:
+                self.__coat_list.remove(other.get_name())
+        elif other.__class__ is Costume:
+            if add:
+                self.__costume_list.append(other.get_name())
+            else:
+                self.__costume_list.remove(other.get_name())
+        else:
+            raise ValueError("Unknow cloth  type")
+
+    @length.setter
+    def length(self, in_len: float):
+        self.__length += in_len
+
+    def __iadd__(self, other):
+        self.length = other.material_length()
+        self.node_list = other
+        return self
+
+    def __isub__(self, other):
+        self.length = - other.material_length()
+        if other.__class__ is Coat and other.get_name() in self.__costume_list:
+            self.__costume_list.remove(other.get_name())
+        elif other.__class__ is Costume and other.get_name() in self.__coat_list:
+            self.__coat_list.remove(other.get_name())
+        return self
 
 
 class ACloth(ABC):
-    """ Abstract class """
-    __name: str
-    __type: 'ClothType'
+    name: str
 
-    def __init__(self, name: str, cloth_type: 'ClothType') -> None:
-        self.__name = name
-        self.__type = cloth_type
+    def get_name(self):
+        return self.name
 
     @abstractmethod
-    def calc_cloth(self) -> float:
-        """ calc how mutch cloth need """
+    def material_length(self):
+        """ return length of material """
 
 
 class Coat(ACloth):
-    __size: float
 
-    def __init__(self, name: str, size: float) -> None:
-        super().__init__(name, ClothType.COAT)
-        self.__size = size
+    def __init__(self, name, size) -> None:
+        super().__init__()
+        self.name = name
+        self.size = size
 
-    def calc_cloth(self) -> float:
-        return self.__size / 6.5 + 0.5
+    def material_length(self):
+        return self.size * 6.5 + 0.5
 
 
 class Costume(ACloth):
-    __height: float
 
-    def __init__(self, name: str, height) -> None:
-        super().__init__(name, ClothType.COSTUME)
-        self.__height = height
+    def __init__(self, name, height) -> None:
+        super().__init__()
+        self.name = name
+        self.height = height
 
-    def calc_cloth(self) -> float:
-        return 2 * self.__height + 0.3
+    def material_length(self):
+        return 2 * self.height + 0.3
+
+
+if __name__ == "__main__":
+
+    mtr_solver = MaterialLenSolver()
+
+    mtr_solver += Coat("For user 001", 10)
+    mtr_solver += Costume("For film 01", 121)
+    mtr_solver += Costume("For film 02", 119)
+    mtr_solver += Costume("For film 03", 97)
+    mtr_solver += Costume("For film 03", 130)
+    mtr_solver += Costume("For film 04", 180)
+    mtr_solver += Costume("For film 05", 103)
+    mtr_solver += Coat("For user 111", 15)
+    mtr_solver += Coat("For user 101", 19)
+    mtr_solver += Coat("For user 011", 12)
+
+    print(f"{mtr_solver.length} meters for {mtr_solver.node_list}")
+
+
